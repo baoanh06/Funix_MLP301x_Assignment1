9ca15f68709ee6e5e8154d8f70ad555a0300bf2c
baoanh06 <79512600+baoanh06@users.noreply.github.com>
2025-02-27 21:01:08 +0700

    marking grades

--- BaoAnh_Tran_grade_the_exams.py
+++ BaoAnh_Tran_grade_the_exams.py
@@ -1,12 +1,13 @@
+import numpy as np
 import re
 
-def analyzeData(classExam):
-    # Phân tích
+# Kiểm tra dữ liệu
+def verifyData(classExam):
     print("**** ANALYZING ****")
-    validExam = 0
-    invalidExam = 0
+    numValidExam = 0
+    numInvalidExam = 0
 
-    for studentExam in classExam:
+    for studentExam in classExam[:]:
         exam = studentExam.split(',')
         studentId = exam[0]
         studentAnswer = exam[1:len(exam)]
@@ -20,26 +21,64 @@ def analyzeData(classExam):
                 print("Invalid line of data: N# is invalid:\n")
                 print(studentExam)
 
-            invalidExam += 1
+            numInvalidExam += 1
+            classExam.remove(studentExam)
         else:
-            validExam += 1
+            numValidExam += 1
 
-    if invalidExam == 0:
+    if numInvalidExam == 0:
         print("No errors found!")
 
+    return numValidExam, numInvalidExam, classExam
+
+# Chấm bài
+def markingScore(validExams):
+    answer_key = "B,A,D,D,C,B,D,A,C,C,D,B,A,B,A,C,B,D,A,C,A,A,B,D,D"
+    answer_list = answer_key.split(',')
+
+    scoreList = []
+
+    for studentExam in validExams:
+        exam = studentExam.split(',')
+        studentAnswer = exam[1:len(exam)]
+
+        score = 0
+
+        for i in range(0, len(studentAnswer)):
+            if studentAnswer[i] == answer_list[i]:
+                score += 4
+            elif studentAnswer[i] == '':
+                continue
+            else:
+                score -= 1
+
+        scoreList.append(score)
+
+    return scoreList
+
+# Báo cáo kết quả
+def report(numValidExam, numInvalidExam, scoreList):
     print("**** REPORT ****")
-    print("Total valid lines of data: ", validExam)
-    print("Total invalid lines of data: ", invalidExam)
+    print("Total valid lines of data: ", numValidExam)
+    print("Total invalid lines of data: ", numInvalidExam)
+    print("Mean (average) score: ", np.mean(scoreList))
+    print("Highest score: ", np.max(scoreList))
+    print("Lowest score: ", np.min(scoreList))
+    print("Range of scores: ", np.max(scoreList) - np.min(scoreList))
+    print("Median score: ", np.median(scoreList))
 
 if __name__ == '__main__':
     filename = input("Enter a class file to grade (i.e. class1 for class1.txt): ")
 
     while filename != '-1':
         try:
-            with open(filename+".txt", 'r') as file:
+            with open(filename + ".txt", 'r') as file:
                 print("Successfully opened {}.txt".format(filename))
                 classData = file.readlines()
-                analyzeData(classData)
+
+                numValidExam, numInvalidExam, verifiedClassExam = verifyData(classData)
+                classGrade = markingScore(verifiedClassExam)
+                report(numValidExam,numInvalidExam,classGrade)
         except FileNotFoundError:
             print("File cannot be found.")
         finally:
